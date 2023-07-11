83fd974984c017b31056033a59a92b9637dbfb89
soukainabi <birich.aj@gmail.com>
2023-07-11 15:10:20 -0700

    c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,31 +1,31 @@
 #include "main.h"
+
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
+ * _strncat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to append from src
  *
- * Return: dest
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+char *ptr = dest;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+while (*ptr != '\0')
+{
+ptr++;
 }
 
+while (*src != '\0' && n > 0)
+{
+*ptr = *src;
+ptr++;
+src++;
+n--;
+}
+
+*ptr = '\0';
+
+return (dest);
+}
